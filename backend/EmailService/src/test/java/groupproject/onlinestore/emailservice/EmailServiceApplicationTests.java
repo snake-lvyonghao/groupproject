39675b04e2f717ec11ab2b7b762826f170398774
Shawn Li <shli0267@uni.sydney.edu.au>
2024-10-03 22:08:10 +1000

    Initialize the Email Service App

---
+++ backend/EmailService/src/test/java/groupproject/onlinestore/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package groupproject.onlinestore.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
