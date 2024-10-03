e9637477ea3f91fbfd8e175884536009c0e6e898
Shawn Li <shli0267@uni.sydney.edu.au>
2024-10-03 22:05:32 +1000

    Initialize the Bank App

---
+++ backend/Bank/src/test/java/groupproject/onlinestore/bank/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package groupproject.onlinestore.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
