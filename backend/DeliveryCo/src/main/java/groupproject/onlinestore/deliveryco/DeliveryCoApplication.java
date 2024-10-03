f642adf48ad7add6d1c46b940b24eedb6ba45263
Shawn Li <shli0267@uni.sydney.edu.au>
2024-10-03 22:07:00 +1000

    Initialize the DeliveryCo App

---
+++ backend/DeliveryCo/src/main/java/groupproject/onlinestore/deliveryco/DeliveryCoApplication.java
@@ -0,0 +1,13 @@
+package groupproject.onlinestore.deliveryco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryCoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryCoApplication.class, args);
+    }
+
+}
---
+++ backend/DeliveryCo/src/test/java/groupproject/onlinestore/deliveryco/DeliveryCoApplicationTests.java
@@ -0,0 +1,13 @@
+package groupproject.onlinestore.deliveryco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryCoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
