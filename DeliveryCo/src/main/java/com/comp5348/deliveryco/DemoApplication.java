4fcd7d72d7ac1a3fb08bebad7f54bfa76f34c106
Yonghao Lyu <ylyu0782@uni.sydney.edu.au>
2024-10-14 17:05:20 +1100

    re-allcate architecher

--- DeliveryCo/src/main/java/com/comp5348/deliveryco/DemoApplication.java
+++ DeliveryCo/src/main/java/com/comp5348/deliveryco/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.comp5348.deliveryco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- DeliveryCo/src/test/java/com/comp5348/bank/DemoApplicationTests.java
+++ DeliveryCo/src/test/java/com/comp5348/bank/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.comp5348.bank;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
