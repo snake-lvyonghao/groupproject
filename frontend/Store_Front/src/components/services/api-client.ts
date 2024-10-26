97d51e919b0181d66f02518f5cfa2b55e5a26f53
Shawn <ylyu0782@uni.sydney.edu.au>
2024-10-26 22:36:55 +1100

    add authrization

--- frontend/Store_Front/src/components/services/api-client.ts
+++ frontend/Store_Front/src/components/services/api-client.ts
@@ -1,7 +1,18 @@
 import axios from "axios";
 
+const username="user";
+const password="123";
+
+const authString=`${username}:${password}`;
+const base64AuthString=btoa(authString);
+
+
 export default axios.create({
   //在这里填入API的根目录
-  baseURL: "http://localhost:5000/api/",
+  baseURL: "http://localhost:8080/api/",
   params: {},
+  headers:{
+    "Authorization": `Basic ${base64AuthString}`,
+    "Content-Type": "application/json"  
+  }
 });
